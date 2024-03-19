bcb4adcf6996327574d760d0ccc8fa6ff8952ad8
LucasRossatto <lucas.sant.anagn3@gmail.com>
2024-03-19 14:09:46 -0300

    main 1.0 feat vitoria

--- script.js
+++ script.js
@@ -3,15 +3,15 @@ let JogadorAtivo = 0;
 let currentScore = 0;
 var Points;
 
-const ScoreAtual0 = document.getElementById('current--0');
-const ScoreAtual1 = document.getElementById('current--1');
-const DiceImage = document.getElementById('ImagemDado');
-const HoldBtn = document.getElementById('HoldPoints');
-const ScoreTotal0 = document.getElementById('score--0');
-const ScoreTotal1 = document.getElementById('score--1');
+const ScoreAtual0 = document.getElementById("current--0");
+const ScoreAtual1 = document.getElementById("current--1");
+const DiceImage = document.getElementById("ImagemDado");
+const HoldBtn = document.getElementById("HoldPoints");
+const ScoreTotal0 = document.getElementById("score--0");
+const ScoreTotal1 = document.getElementById("score--1");
 
-const roll = document.getElementById('RollDice');
-const hold = document.getElementById('HoldPoints');
+const roll = document.getElementById("RollDice");
+const hold = document.getElementById("HoldPoints");
 
 function proximoJogador() {
   JogadorAtivo === 0 ? (JogadorAtivo = 1) : (JogadorAtivo = 0);
@@ -20,7 +20,7 @@ function proximoJogador() {
   ScoreAtual1.textContent = currentScore;
 }
 
-roll.addEventListener('click', function () {
+roll.addEventListener("click", function () {
   Points = Math.floor(Math.random() * 6) + 1;
 
   if (Points === 1) {
@@ -33,16 +33,22 @@ roll.addEventListener('click', function () {
   } else {
     ScoreAtual1.textContent = currentScore;
   }
-  DiceImage.setAttribute('src', `dice-${Points}.png`);
+  DiceImage.setAttribute("src", `dice-${Points}.png`);
 });
 
-HoldBtn.addEventListener('click', function () {
+HoldBtn.addEventListener("click", function () {
   if (JogadorAtivo === 0) {
     scores[0] += parseInt(ScoreAtual0.textContent);
     ScoreTotal0.textContent = scores[0];
+    if (scores[0] >= 100) {
+      alert("The player 1 won");
+    }
   } else {
     scores[1] += parseInt(ScoreAtual1.textContent);
     ScoreTotal1.textContent = scores[1];
+  }
+  if (scores[1] >= 100) {
+    alert("The player 2 won");
   }
   proximoJogador();
 });
